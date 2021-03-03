555095cd435c2c9cf49dbdbea403a945f6264c18
Davide Carella <carelladavide1@gmail.com>
2021-03-03 18:05:57 +0100

    Tracer: Moved all ray-tracing related code to a separate class called Tracer, that will give a single function called by 'main' to create a render.

---
+++ src/Tracer.cpp
@@ -0,0 +1,49 @@
+#include "Tracer.hpp"
+
+Tracer::Tracer(size_t imageWidth, size_t imageHeight):
+	m_renderImage(imageWidth, imageHeight),
+	m_aspectRatio((double) imageWidth / imageHeight)
+{
+	// FIXME: Right now we assume that the aspect ratio is greater or equal to 1 meaning that the image width is greater than the image height.
+
+	double viewportHeight = 2.0;
+	double viewportWidth = viewportHeight * m_aspectRatio;
+	double focalLength = 3.0;
+
+	m_iamgePlane[0] = Vec3(-viewportWidth / 2, -viewportHeight / 2, -focalLength); // Bottom-Left Corner
+	m_iamgePlane[1] = Vec3( viewportWidth / 2, -viewportHeight / 2, -focalLength); // Bottom-Right Corner
+	m_iamgePlane[2] = Vec3(-viewportWidth / 2,  viewportHeight / 2, -focalLength); // Top-Left Corner
+	m_iamgePlane[3] = Vec3( viewportWidth / 2,  viewportHeight / 2, -focalLength); // Top-Right Corner
+}
+
+void Tracer::render() {
+	for (size_t y = 0; y < m_renderImage.height(); ++y) {
+		for (size_t x = 0; x < m_renderImage.width(); ++x) {
+			Vec3 color = trace_pixel(x, y);
+			auto rgb = PPMImage::rgb_from_vector(color);
+			m_renderImage.set_pixel(x, y, rgb);
+		}
+	}
+
+	m_renderImage.save("render.ppm");
+}
+
+Ray Tracer::get_ray(size_t x, size_t y) {
+	if (x >= m_renderImage.width() && y >= m_renderImage.height()) return Vec3(-1, -1, -1);
+
+	double u = (double) x / m_renderImage.width();
+	double v = (double) (m_renderImage.height() - y - 1) / m_renderImage.height();
+
+	Vec3 b = Vec3::lerp(m_imagePlane[0], m_iamgePlane[1], u);
+	Vec3 t = Vec3::lerp(m_imagePlane[2], m_iamgePlane[3], u);
+	Vec3 p = Vec3::lerp(b, t, v);
+
+	// FIXME: The value of the "eye" position shouldn't be hardcoded
+	return Ray(p - Vec3(0, 0, 0), p);
+}
+
+Vec3 Tracer::trace_pixel(size_t x, size_t y) {
+	if (x >= m_renderImage.width() && y >= m_renderImage.height()) return Vec3(-1, -1, -1);
+
+	Ray ray = get_ray(x, y);
+}
---
+++ src/Tracer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stddef.h>
+#include "PPM.hpp"
+
+class Tracer {
+	public:
+		Tracer(size_t imageWidth, size_t imageHeight);
+
+		void render();
+	private:
+		Vec3 trace_pixel(size_t x, size_t y);
+		Ray get_ray(size_t x, size_t y);
+
+		PPMImage m_renderImage;
+		double m_aspectRatio;
+		Vec3 m_imagePlane[4];
+};
