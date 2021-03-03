cdbaac7fc96abdc997cf889fe6691ada52fef0bd
Davide Carella <carelladavide1@gmail.com>
2021-03-03 18:28:58 +0100

    Ray: Added 'const' qualifiers to the getters of origin and direction.

--- src/Math/Ray.hpp
+++ src/Math/Ray.hpp
@@ -8,8 +8,8 @@ class Ray {
 		Ray(const Vec3& origin, const Vec3& direction):
 			m_origin(origin), m_direction(direction) {}
 
-		Vec3 origin() { return m_origin; }
-		Vec3 direction() { return m_direction; }
+		Vec3 origin() const { return m_origin; }
+		Vec3 direction() const { return m_direction; }
 
 		Vec3 at(double t) const { return origin() + t * direction(); }
 	private:
