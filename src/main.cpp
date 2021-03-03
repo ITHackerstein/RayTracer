831f642047a528a3bc59e0b433ecc40ab8c64f4d
Davide Carella <carelladavide1@gmail.com>
2021-03-03 18:32:21 +0100

    Main: Now using the Tracer class and its 'render' method.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,7 @@
 #include <stdio.h>
-#include "PPM.hpp"
-#include "Math/Vec3.hpp"
+#include "Tracer.hpp"
 
 int main() {
-	PPMImage img (640, 480);
-
-	for (size_t j = 0; j < img.height(); ++j) {
-		printf("\rRow %zu / %zu", j, img.height());
-		fflush(stdout);
-		for (size_t i = 0; i < img.width(); ++i) {
-			auto col = PPMImage::rgb_from_vector(Vec3::lerp(Vec3(0.1, 0.2, 0.8), Vec3(1, 0, 0), (double)j / img.height()));
-			img.set_pixel(i, j, col);
-		}
-	}
-	printf("\nDone!\n");
-
-	img.save("render.ppm");
+	Tracer t (640, 480);
+	t.render();
 }
