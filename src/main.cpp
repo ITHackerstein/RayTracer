38ade684397c598aa974d1fd1c9557a90a9b5f7a
Davide Carella <carelladavide1@gmail.com>
2021-03-05 13:09:31 +0100

    Main: Added some materials to the sphere.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 #include "Primitives/HittableList.hpp"
 #include "Primitives/Sphere.hpp"
 #include "Tracer.hpp"
+#include "Materials/LambertianDiffuse.hpp"
 
 int main() {
 	HittableList scene;
-	scene.add(std::make_shared<Sphere>(Vec3(0, 0, -4), 0.5));
-	scene.add(std::make_shared<Sphere>(Vec3(0, -100.5, -4), 100));
+	scene.add(std::make_shared<Sphere>(Vec3(0, 0, -4), 0.5, std::make_shared<LambertianDiffuse>(Vec3(0.8, 0.3, 0.2))));
+	scene.add(std::make_shared<Sphere>(Vec3(0, -100.5, -4), 100, std::make_shared<LambertianDiffuse>(Vec3(0.3, 0.3, 0.3))));
 
 	Vec3 camera (0, 0, 0);
 	Tracer t (640, 480, 50, camera, scene);
