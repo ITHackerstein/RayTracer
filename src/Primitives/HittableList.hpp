fb983841c24d10e8e300aaff730dd9ec8252e11e
Davide Carella <carelladavide1@gmail.com>
2021-03-06 10:39:00 +0100

    HittableList: Added a 'size' method that returns the number of objects in the list. Main: Now we handle errors in the TOML scene file, we take the scene file as an argument to the executable and we log details about the scene.

--- src/Primitives/HittableList.hpp
+++ src/Primitives/HittableList.hpp
@@ -9,6 +9,7 @@ class HittableList : public Hittable {
 		HittableList() {}
 
 		void add(std::shared_ptr<Hittable> obj) { m_objects.push_back(obj); }
+		size_t size() const { return m_objects.size(); }
 
 		virtual bool intersects_ray(const Ray&, double tMin, double tMax, HitRecord&) const override;
 	private:
--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,106 @@
-#include <iostream>
+#define TOML_EXCEPTIONS 0
+
 #include <fstream>
+#include <iostream>
+#include <stdlib.h>
 #include <string_view>
+#include <toml++/toml.h>
+
+#include "Materials/LambertianDiffuse.hpp"
+#include "Materials/Metal.hpp"
 #include "Primitives/HittableList.hpp"
 #include "Primitives/Sphere.hpp"
 #include "Tracer.hpp"
-#include "Materials/LambertianDiffuse.hpp"
-#include "Materials/Metal.hpp"
 
-#define TOML_EXCEPTIONS 0
-#include <toml++/toml.h>
+static toml::node* get_key_or_error(toml::table& table, const char* key) {
+	auto node = table.get(key);
+	if (node)
+		return node;
 
-static Vec3 parse_vector_array(toml::array* vector_array) {
-	Vec3 v(
-		*(vector_array->get(0)->value<double>()),
-		*(vector_array->get(1)->value<double>()),
-		*(vector_array->get(2)->value<double>())
-	);
-
-	return v;
+	std::cerr << "[TOML] Key '" << key << "' not found\n";
+	exit(1);
 }
 
-static std::shared_ptr<Material> parse_material(toml::table* table) {
-	auto material_type = *(table->get("type")->value<std::string_view>());
-	if (material_type == "LambertianDiffuse") {
-		Vec3 albedo = parse_vector_array(table->get("albedo")->as_array());
+template<typename T>
+static T get_variable_or_error(toml::table& table, const char* variableName) {
+	auto variable = get_key_or_error(table, variableName)->value<T>();
+	if (variable)
+		return *variable;
+
+	std::cerr << "[TOML] Variable '" << variableName << "' not found!\n";
+	exit(1);
+}
+
+static toml::array get_array_or_error(toml::table& table, const char* arrayName) {
+	auto arr = get_key_or_error(table, arrayName)->as_array();
+	if (arr)
+		return *arr;
+
+	std::cerr << "[TOML] Array '" << arrayName << "' not found!\n";
+	exit(1);
+}
+
+static toml::table get_table_or_error(toml::table& table, const char *tableName) {
+	auto childTable = get_key_or_error(table, tableName)->as_table();
+	if (childTable)
+		return *childTable;
+
+	std::cerr << "[TOML] Table '" << tableName << "' not found!\n";
+	exit(1);
+}
+
+static Vec3 parse_vector_array(toml::array& vectorArray) {
+	if (vectorArray.size() < 3) {
+		std::cerr << "[TOML] Invalid vector array!\n";
+		exit(1);
+	}
+
+	auto x = vectorArray.get(0)->value<double>();
+	auto y = vectorArray.get(1)->value<double>();
+	auto z = vectorArray.get(2)->value<double>();
+	if (!x || !y || !z) {
+		std::cerr << "[TOML] Invalid vector array!\n";
+		exit(1);
+	}
+
+	return Vec3(*x, *y, *z);
+}
+
+static std::shared_ptr<Material> parse_material(toml::table& table) {
+	auto materialType = get_variable_or_error<std::string_view>(table, "type");
+	if (materialType == "LambertianDiffuse") {
+		auto albedoArray = get_array_or_error(table, "albedo");
+		Vec3 albedo = parse_vector_array(albedoArray);
+
 		return std::make_shared<LambertianDiffuse>(albedo);
 	}
 
-	if (material_type == "Metal") {
-		Vec3 albedo = parse_vector_array(table->get("albedo")->as_array());
-		double fuzzyness = *(table->get("fuzzyness")->value<double>());
+	if (materialType == "Metal") {
+		auto albedoArray = get_array_or_error(table, "albedo");
+		Vec3 albedo = parse_vector_array(albedoArray);
+
+		double fuzzyness = get_variable_or_error<double>(table, "fuzzyness");
+
 		return std::make_shared<Metal>(albedo, fuzzyness);
 	}
 
 	return nullptr;
 }
 
-static std::shared_ptr<Hittable> parse_hittable_object(toml::table* table) {
-	auto object_type = *(table->get("type")->value<std::string_view>());
-	if (object_type == "Sphere") {
-		Vec3 center = parse_vector_array(table->get("center")->as_array());
-		double radius = *(table->get("radius")->value<double>());
-		auto material = parse_material(table->get("material")->as_table());
+static std::shared_ptr<Hittable> parse_hittable_object(toml::table& table) {
+	auto objectType = get_variable_or_error<std::string_view>(table, "type");
+	if (objectType == "Sphere") {
+		auto centerArray = get_array_or_error(table, "center");
+		Vec3 center = parse_vector_array(centerArray);
+
+		double radius = get_variable_or_error<double>(table, "radius");
+
+		auto materialTable = get_table_or_error(table, "material");
+		auto material = parse_material(materialTable);
+		if (!material) {
+			std::cerr << "[RayTracer] Invalid material!\n";
+			exit(1);
+		}
 
 		return std::make_shared<Sphere>(center, radius, material);
 	}
@@ -49,29 +108,56 @@ static std::shared_ptr<Hittable> parse_hittable_object(toml::table* table) {
 	return nullptr;
 }
 
-int main() {
-	auto res = toml::parse_file("../scene.toml");
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " <scene-file>\n";
+		return 1;
+	}
+
+	char* fileName = argv[1];
+
+	auto res = toml::parse_file(fileName);
 	if (!res) {
 		std::cerr << "[TOML] Error while parsing scene file!\n  " << res.error() << "\n";
 		return 1;
 	}
 
 	auto table = res.table();
-	std::cout << "Rendering scene: " << *(table["scene_title"].value<std::string_view>()) << "\n";
 
-	size_t width = *(table["width"].value<size_t>());
-	size_t height = *(table["height"].value<size_t>());
-	size_t samples = *(table["samples"].value<size_t>());
+	auto sceneTitle = get_variable_or_error<std::string_view>(table, "scene_title");
+	auto width = get_variable_or_error<size_t>(table, "width");
+	auto height = get_variable_or_error<size_t>(table, "height");
+	auto samples = get_variable_or_error<size_t>(table, "samples");
 
-	Vec3 camera = parse_vector_array(table["camera"].as_array());
+	auto cameraArray = get_array_or_error(table, "camera");
+	Vec3 camera = parse_vector_array(cameraArray);
 
 	HittableList scene;
 
-	auto objects_array = table["objects"].as_array();
-	for (auto& object_node: *objects_array) {
-		auto object = object_node.as_table();
-		scene.add(parse_hittable_object(object));
+	auto objectsArray = get_array_or_error(table, "objects");
+	for (auto& objectNode: objectsArray) {
+		auto object = objectNode.as_table();
+		if (!object) {
+			std::cerr << "[TOML] Invalid object!\n";
+			exit(1);
+		}
+
+		auto hittableObject = parse_hittable_object(*object);
+		if (!hittableObject) {
+			std::cerr << "[RayTracer] Invalid object!\n";
+			exit(1);
+		}
+
+		scene.add(parse_hittable_object(*object));
 	}
+
+	std::cout << "[RayTracer] Rendering scene:\n";
+	std::cout << "  Title: " << sceneTitle << "\n";
+	std::cout << "  Width: " << width << "\n";
+	std::cout << "  Height: " << height << "\n";
+	std::cout << "  Samples per pixel: " << samples << "\n";
+	std::cout << "  Camera: [ " << camera.x << ", " << camera.y << ", " << camera.z << " ]\n";
+	std::cout << "  " << scene.size() << " objects\n";
 
 	Tracer t (width, height, samples, camera, scene);
 	t.render();
